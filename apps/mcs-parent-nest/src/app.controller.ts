b5af3db70f7285fc7c6b11b67bd3796158e4f0fb
daniel juliao <zigmainflables@gmail.com>
2024-11-15 16:05:09 -0500

    mejoras in pagina de inicio

---
+++ apps/mcs-parent-nest/src/app.controller.ts
@@ -0,0 +1,10 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+
+  @Get('/')
+  getHome(): string {
+    return '<h1> Welcome to my API! </h1>';
+  }
+}
--- apps/mcs-parent-nest/src/app.module.ts
+++ apps/mcs-parent-nest/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppService } from './app.service';
 import { WebhookModule } from './modulos/webhook.module';
+import { AppController } from './app.controller';
 
 @Module({
   imports: [WebhookModule],
   providers: [AppService],
-  controllers: [],
+  controllers: [AppController],
 })
 export class AppModule {}
